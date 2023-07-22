f28372831b50993459a7ac914d1ee25e22a71311
Tiago Behenck <tiago.behenck.santos@gmail.com>
2023-07-22 18:28:42 -0300

    create the header to show creates todos and done todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Header } from './components'
+import { Header, Input } from './components'
 
 import './global.css'
+import styles from './app.module.css'
 
 export default function App() { 
-  return <Header />
+  return (
+    <>
+      <Header />
+      <section className={styles.wrapper}>
+        <Input />
+        <main>
+          <head>
+            <p className={styles.todosCreated}>
+              Tarefas criadas 
+              <span className={styles.counter}>0</span>
+            </p>
+            <p className={styles.todosDone}>
+              Concluídas 
+              <span className={styles.counter}>0</span>
+            </p>
+          </head>
+        </main>
+      </section>
+    </>
+  )
 }
